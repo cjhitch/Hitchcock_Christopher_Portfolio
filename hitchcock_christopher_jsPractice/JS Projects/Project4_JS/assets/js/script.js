41f865b13dc6a091fae4068948453e9b924e7a22
Christopher Hitchcock <chitchcock@Christophers-Mac-mini.local>
2019-09-15 10:48:41 -0700

    coffee run calculator done

--- hitchcock_christopher_jsPractice/JS Projects/Project4_JS/assets/js/script.js
+++ hitchcock_christopher_jsPractice/JS Projects/Project4_JS/assets/js/script.js
@@ -1,44 +1,35 @@
 (function(){
 
-    // variables for the program
-    let firstArray = [34, 20, 91, 49];
-    let secondArray = [42, 120.30, 210.20, 32.50];
-    var sumArrayOne = 0;
-    var sumArrayTwo = 0;
-    let newCombinedArray = [];
+    let coffeeOrderName = ["coffee", "cappuccino", "latte", "cappuccino", "latte", "coffee", "cappuccino", "coffee", "decaf", "cappuccino"];
+    let coffeeNames = ["coffee", "cappucino", "latte", "decaf"];
+    let coffeeOrderNum = [0,0,0,0];
 
-    // sum of arrays
-    firstArray.forEach(element => {
-        sumArrayOne += element;
-    });
-    // write out to the console
-    console.log("Sum of Array One: ",sumArrayOne,"\r\nArray One's Length: ", firstArray.length);
-
-    // sum of second array
-    secondArray.forEach(element =>{
-        sumArrayTwo += element;
-    });
-    // write to console
-    console.log("\nSum of Array Two: ", sumArrayTwo,"\r\nArray Two's Length: ", secondArray.length);
-
-    // get avg and print to console of both arrays
-    let arrayOneAvg = sumArrayOne / firstArray.length;
-    console.log("\nAverage of Array One: ", arrayOneAvg);
-
-    let arrayTwoAvg = sumArrayTwo / secondArray.length;
-    console.log("\nAverage of Array Two: ", arrayTwoAvg,"\n");
-
-    // combine arrays into one array
-    for (let i = 0; i < firstArray.length; i++) {
-        newCombinedArray[i] = firstArray[i] + secondArray[i];
-        console.log(`New Combined Array Value at Index ${i}: `,newCombinedArray[i]);
+    console.log("Thank you for using our coffee run calculator. This program will tell you how many of each drink to order!")
+    console.log("\n")
+    for (let i = 0; i < coffeeOrderName.length; i++) {
+        if (coffeeOrderName[i] == "coffee"){
+            coffeeOrderNum[0] += 1;
+        }
+        else if (coffeeOrderName[i] == "cappuccino"){
+            coffeeOrderNum[1] += 1;
+        }
+        else if (coffeeOrderName[i] == "latte"){
+            coffeeOrderNum[2] += 1;
+        }
+        else{
+            coffeeOrderNum[3] += 1;
+        }   
     }
 
-    // mixed up string array
-    let  mixedUp = ["universe is winning.", "erse trying to produce bigger an", "between software engineers striving to build bigger ", "d better idiots. So far, the ", "Programming today is a race ", "and better idiot-proof programs, and the univ"];
+    for (let i = 0; i < coffeeOrderNum.length; i++) {
+        if (coffeeOrderNum[i]>1){
+            console.log(`Order ${coffeeOrderNum[i]} ${coffeeNames[i]}s`);
+        }
+        else {
+            console.log(`Order ${coffeeOrderNum[i]} ${coffeeNames[i]}`);
+        }
+    }
 
-    // fixed string array and print to console
-    let fixedSentence =  mixedUp[4] + mixedUp[2] + mixedUp[5] + mixedUp[1] + mixedUp[3] + mixedUp[0];
-    console.log("\n",fixedSentence);
+    console.log("\nThank you for using our coffee run calculator! Please use our app again in the future!")
 
 })();
